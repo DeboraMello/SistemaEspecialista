f6853d4a2c606a77f5b43c7e2963167606117005
Débora Mello <debora.mello@upflow.me>
2021-08-05 20:33:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sistemaEspecialista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
