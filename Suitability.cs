f1d77bd0c792fff45f72880c324ae0210aeca00a
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2021-08-06 22:03:51 -0300

    Adicionando Classes de Exemplo
    Adicionando algumas classes de exemplo para construção da estrutura de objetos no sistema especialista. Não precisa ser usado obrigatoriamente. É apenas um exemplo.

--- Suitability.cs
+++ Suitability.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sistemaEspecialista
 {
     class Suitability
     {
+            
+    }
 
+    public class Interacao
+    {
+        public int Id { get; set; }
+        public string Pergunta { get; set; }
+        public List<Resposta> Respostas { get; set; }
+    }
+
+    public class Resposta
+    {
+        public int Pontos { get; set; }
+        public string Texto { get; set; }
+        public Interacao ProximaIteracao { get; set; }
     }
 }
+
